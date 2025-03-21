46ed17e4fd62fc4359342cbb3e3f21034c9f1fd3
Dimm <dimaspradiptam@gmail.com>
2025-03-22 06:09:53 +0700

    Rebuilding

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./src/**/*.{html,js}", // Perbaikan jalur
+    "./public/**/*.blade.php", // Menggunakan ** untuk folder
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        satu: "#028174",
+        dua: "#0ab68b",
+        tiga: "#92de8b",
+        empat: "#ffe3b3",
+        putih: "#f7f7f7",
+      },
+      scrollBehavior: ["smooth"],
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "cyberpunk", "dim", "dracula"],
+  },
+};
